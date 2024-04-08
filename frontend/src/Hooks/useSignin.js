8677d83b9d52517942d491c73fa183455e23af47
codeshaine <shainilps05@gmail.com>
2024-04-08 14:59:05 +0530

    fixed frontend

--- frontend/src/Hooks/useSignin.js
+++ frontend/src/Hooks/useSignin.js
@@ -4,81 +4,70 @@ import { useNavigate } from "react-router-dom";
 // import toast, { Toaster } from 'react-hot-toast';
 
 function useSignin() {
-    const [loading, setLoading] = useState(false);
-    const navigate = useNavigate()
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-    const signinverify = async ({ email, password }) => {
-        const pass = errhandle(email, password);
-        setLoading(true);
+  const signinverify = async ({ email, password }) => {
+    const pass = errhandle(email, password);
+    setLoading(true);
 
-        if (!pass) {
-            setLoading(false);
-            return;
-        }
+    if (!pass) {
+      setLoading(false);
+      return;
+    }
 
-        try {
-            const response = await axios.post('/user/login', {
-                email,
-                password,
-            });
-            console.log(response.data.sucess);
+    try {
+      const response = await axios.post("api/user/login", {
+        email: "swasthikk@gmail.com",
+        password: "77484",
+      });
+      console.log(response.data);
 
-            if (response.status === 201) {
-                console.log(response.data);
-                navigate('/register')
-            } else {
-                // Handle error status codes
-                switch (response.status) {
-                    case 404:
-                        toast.error(response.data.message);
-                        console.log('Error 404: ' + response.data.message);
-                        break;
-                    case 409:
-                        toast.error(response.data.message);
-                        console.log('Error 409: ' + response.data.message);
-                        break;
-                    default:
-                        throw new Error('Error: ' + response.data.message)
-                }
-                // navigate('/register')
-            }
-        } catch (error) {
-            console.log(error.message);
-            if (error.response && error.response.data) {
-                toast.error(error.response.data.message);
-                console.log(error.response.data.message);
-            } else {
-                toast.error(error.message);
-                console.log(error.message);
-            }
-        } finally {
-            setLoading(false);
-        }
-    };
+      if (response.status === 200) {
+        console.log(response.data);
+        navigate("/register");
+      } else {
+        // Handle error status codes
+        // navigate("/register");
+        console.log("else somehting");
+      }
+    } catch (error) {
+      console.log(error.message);
+      if (error.response && error.response.data) {
+        toast.error(error.response.data.message);
+        console.log(error.response.data.message);
+      } else {
+        toast.error(error.message);
+        console.log(error.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    return { loading, signinverify };
+  return { loading, signinverify };
 }
 
 function errhandle(email, password) {
-    if (!email || !password) {
-        // toast.error("Enter all details");
-        console.log("enter all details");
-        return false;
-    }
+  if (!email || !password) {
+    // toast.error("Enter all details");
+    console.log("enter all details");
+    return false;
+  }
 
-    if (password.length < 6) {
-        // toast.error("Password should have at least 6 characters");
-        console.log("6 char must");
-        return false;
-    }
+  if (password.length < 6) {
+    // toast.error("Password should have at least 6 characters");
+    console.log("6 char must");
+    return false;
+  }
 
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-        // toast.error("Enter a valid email address");
-        console.log("not an email");
-        return false;
-    }
-    return true;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    // toast.error("Enter a valid email address");
+    console.log("not an email");
+    return false;
+  }
+  return true;
 }
 
 export default useSignin;
